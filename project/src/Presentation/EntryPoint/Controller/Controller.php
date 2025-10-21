85accc6ee4a56aea8df6191ec4460c2aaa9025df
Nikita Ponyavin <nikita_ponyavin@mail.ru>
2025-10-22 06:17:46 +0700

    Доработан базовый шаблон. Подключен бутстрап 5. Добавлены переводы

--- project/src/Presentation/EntryPoint/Controller/Controller.php
+++ project/src/Presentation/EntryPoint/Controller/Controller.php
@@ -4,18 +4,13 @@ namespace App\Presentation\EntryPoint\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Controller extends AbstractController
 {
     #[Route(path: '/', name: 'app_index', methods: ['GET'])]
-    public function __invoke(
-        #[MapQueryParameter] string $name = '',
-    ): Response
+    public function __invoke(): Response
     {
-        return $this->render('app/index.html.twig', [
-            'name' => $name,
-        ]);
+        return $this->render('app/index.html.twig');
     }
 }
