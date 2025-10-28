df9d4e0cfdec0067c6a7c3ba5b9b79ff4022efcc
Nikita Ponyavin <nikita_ponyavin@mail.ru>
2025-10-28 15:26:51 +0700

    Добавлены все поля клиента на страничку просмотра клиента. Для правильного отображения даты добавлен скрипт для сохранения таймзоны в куки и использование этих куков в контроллере и шаблоне.

--- project/src/Presentation/EntryPoint/Controller/Admin/ShowClientController.php
+++ project/src/Presentation/EntryPoint/Controller/Admin/ShowClientController.php
@@ -5,6 +5,7 @@ namespace App\Presentation\EntryPoint\Controller\Admin;
 use App\Application\Client\Connector\Query\ClientEntity\ClientEntityQuery;
 use App\Application\Client\Exception\ClientEntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,10 +13,13 @@ final class ShowClientController extends AbstractController
 {
     #[Route('/admin/client/{id}', name: 'admin_show_client', methods: ['GET'])]
     public function __invoke(
+        Request           $request,
         ClientEntityQuery $clientEntityQuery,
         int               $id
     ): Response
     {
+        $userTimezone = $request->cookies->get('user_timezone', 'UTC');
+
         try {
             $client = $clientEntityQuery->execute($id, true);
         } catch (ClientEntityNotFoundException $e) {
@@ -23,7 +27,8 @@ final class ShowClientController extends AbstractController
         }
 
         return $this->render('admin/showClient.html.twig', [
-            'client' => $client,
+            'client'       => $client,
+            'userTimezone' => $userTimezone,
         ]);
     }
 }
